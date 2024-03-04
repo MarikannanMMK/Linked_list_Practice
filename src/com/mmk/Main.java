f8540479e1d66ebcdada63fc4ab13b685a911854
mariinbumurugan <109297860+mariinbumurugan@users.noreply.github.com>
2024-03-04 18:57:08 +0530

    Singly linked list upto insert

--- src/com/mmk/Main.java
+++ src/com/mmk/Main.java
@@ -3,6 +3,23 @@ package com.mmk;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        LL linkedList = new LL();
+
+        System.out.println("welcome to Linked list");
+
+        linkedList.insert_first(100);
+        linkedList.insert_first(15);
+        linkedList.insert_first(28);
+        linkedList.insert_first(93);
+        linkedList.insert_first(97);
+
+        linkedList.insert_Last(27);
+
+        linkedList.insertPosition(2,88);
+
+        linkedList.display();
+
+
     }
 }
