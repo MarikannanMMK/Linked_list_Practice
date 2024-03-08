f7ba785aaf3402e74b75251cfec2811a03ce79d0
mariinbumurugan <109297860+mariinbumurugan@users.noreply.github.com>
2024-03-08 20:55:57 +0530

    trees upto BST

--- src/com/mmk/trees/BinaryTree.java
+++ src/com/mmk/trees/BinaryTree.java
@@ -72,6 +72,46 @@ public class BinaryTree {
         prettyDisplay(node.left, level + 1);
     }
 
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    public void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.val + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    public void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.println(node.val + " ");
+        inOrder(node.right);
+    }
+
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    public void postOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.val + " ");
+
+    }
+
     private static class Node {
 
         int val;
